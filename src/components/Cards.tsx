d92dae92486f5102f046300d450a965726e9de10
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2024-03-17 12:09:02 +0500

    ./

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Cards() {
   return (
     <div className="container flex gap-[30px] flex-wrap justify-center items-center">
-     <div className="px-[17px] w-[370px] pb-[22px] bg-white">
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
      <Image
         className="min-w-[370px] ml-[-17.5px] mb-[6px]"
         alt=""
@@ -24,14 +24,14 @@ function Cards() {
             <Image src={"/loc.svg"} alt="" width={11} height={14} />
             <p className="text-[14px] underline opacity-60">Маяк Минска</p>
           </div>
-        <div>
-        <Image src={"/m.svg"} alt="" width={11} height={14} />
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
             <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
         </div>
         </div>
       </div>
      </div>
-     <div className="px-[17px] w-[370px] pb-[22px] bg-white">
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
      <Image
         className="min-w-[370px] ml-[-17.5px] mb-[6px]"
         alt=""
@@ -40,7 +40,7 @@ function Cards() {
         height={280}
       />
       <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
-        Ул. Петра Мстиславца, 24
+      Ул. Веры Хоружей, 10/2
       </h4>
       <div className="flex justify-between">
         <div className="text-[14px]">
@@ -52,14 +52,14 @@ function Cards() {
             <Image src={"/loc.svg"} alt="" width={11} height={14} />
             <p className="text-[14px] underline opacity-60">Маяк Минска</p>
           </div>
-        <div>
-        <Image src={"/m.svg"} alt="" width={11} height={14} />
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
             <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
         </div>
         </div>
       </div>
      </div>
-     <div className="px-[17px] w-[370px] pb-[22px] bg-white">
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
      <Image
         className="min-w-[370px] ml-[-17.5px] mb-[6px]"
         alt=""
@@ -68,7 +68,7 @@ function Cards() {
         height={280}
       />
       <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
-        Ул. Петра Мстиславца, 24
+      Пр-т Независимости, 52
       </h4>
       <div className="flex justify-between">
         <div className="text-[14px]">
@@ -80,14 +80,14 @@ function Cards() {
             <Image src={"/loc.svg"} alt="" width={11} height={14} />
             <p className="text-[14px] underline opacity-60">Маяк Минска</p>
           </div>
-        <div>
-        <Image src={"/m.svg"} alt="" width={11} height={14} />
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
             <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
         </div>
         </div>
       </div>
      </div>
-     <div className="px-[17px] w-[370px] pb-[22px] bg-white">
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
      <Image
         className="min-w-[370px] ml-[-17.5px] mb-[6px]"
         alt=""
@@ -108,14 +108,14 @@ function Cards() {
             <Image src={"/loc.svg"} alt="" width={11} height={14} />
             <p className="text-[14px] underline opacity-60">Маяк Минска</p>
           </div>
-        <div>
-        <Image src={"/m.svg"} alt="" width={11} height={14} />
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
             <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
         </div>
         </div>
       </div>
      </div>
-     <div className="px-[17px] w-[370px] pb-[22px] bg-white">
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
      <Image
         className="min-w-[370px] ml-[-17.5px] mb-[6px]"
         alt=""
@@ -124,7 +124,7 @@ function Cards() {
         height={280}
       />
       <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
-        Ул. Петра Мстиславца, 24
+      Ул. Веры Хоружей, 10/2
       </h4>
       <div className="flex justify-between">
         <div className="text-[14px]">
@@ -136,18 +136,46 @@ function Cards() {
             <Image src={"/loc.svg"} alt="" width={11} height={14} />
             <p className="text-[14px] underline opacity-60">Маяк Минска</p>
           </div>
-        <div>
-        <Image src={"/m.svg"} alt="" width={11} height={14} />
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
             <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
         </div>
         </div>
       </div>
      </div>
-     <div className="px-[17px] w-[370px] pb-[22px] bg-white">
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
      <Image
         className="min-w-[370px] ml-[-17.5px] mb-[6px]"
         alt=""
         src={"/third.png"}
+        width={370}
+        height={280}
+      />
+      <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
+      Пр-т Независимости, 52
+      </h4>
+      <div className="flex justify-between">
+        <div className="text-[14px]">
+          <p>Спальных мест: 2</p>
+          <p>Комнат: 1</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-[5px]">
+            <Image src={"/loc.svg"} alt="" width={11} height={14} />
+            <p className="text-[14px] underline opacity-60">Маяк Минска</p>
+          </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
+            <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
+        </div>
+        </div>
+      </div>
+     </div>
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
+     <Image
+        className="min-w-[370px] ml-[-17.5px] mb-[6px]"
+        alt=""
+        src={"/first-img.svg"}
         width={370}
         height={280}
       />
@@ -164,8 +192,148 @@ function Cards() {
             <Image src={"/loc.svg"} alt="" width={11} height={14} />
             <p className="text-[14px] underline opacity-60">Маяк Минска</p>
           </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
+            <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
+        </div>
+        </div>
+      </div>
+     </div>
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
+     <Image
+        className="min-w-[370px] ml-[-17.5px] mb-[6px]"
+        alt=""
+        src={"/second.png"}
+        width={370}
+        height={280}
+      />
+      <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
+      Ул. Веры Хоружей, 10/2
+      </h4>
+      <div className="flex justify-between">
+        <div className="text-[14px]">
+          <p>Спальных мест: 2</p>
+          <p>Комнат: 1</p>
+        </div>
         <div>
-        <Image src={"/m.svg"} alt="" width={11} height={14} />
+          <div className="flex items-center gap-[5px]">
+            <Image src={"/loc.svg"} alt="" width={11} height={14} />
+            <p className="text-[14px] underline opacity-60">Маяк Минска</p>
+          </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
+            <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
+        </div>
+        </div>
+      </div>
+     </div>
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
+     <Image
+        className="min-w-[370px] ml-[-17.5px] mb-[6px]"
+        alt=""
+        src={"/third.png"}
+        width={370}
+        height={280}
+      />
+      <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
+      Пр-т Независимости, 52
+      </h4>
+      <div className="flex justify-between">
+        <div className="text-[14px]">
+          <p>Спальных мест: 2</p>
+          <p>Комнат: 1</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-[5px]">
+            <Image src={"/loc.svg"} alt="" width={11} height={14} />
+            <p className="text-[14px] underline opacity-60">Маяк Минска</p>
+          </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
+            <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
+        </div>
+        </div>
+      </div>
+     </div>
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
+     <Image
+        className="min-w-[370px] ml-[-17.5px] mb-[6px]"
+        alt=""
+        src={"/first-img.svg"}
+        width={370}
+        height={280}
+      />
+      <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
+        Ул. Петра Мстиславца, 24
+      </h4>
+      <div className="flex justify-between">
+        <div className="text-[14px]">
+          <p>Спальных мест: 2</p>
+          <p>Комнат: 1</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-[5px]">
+            <Image src={"/loc.svg"} alt="" width={11} height={14} />
+            <p className="text-[14px] underline opacity-60">Маяк Минска</p>
+          </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
+            <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
+        </div>
+        </div>
+      </div>
+     </div>
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
+     <Image
+        className="min-w-[370px] ml-[-17.5px] mb-[6px]"
+        alt=""
+        src={"/second.png"}
+        width={370}
+        height={280}
+      />
+      <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
+      Ул. Веры Хоружей, 10/2
+      </h4>
+      <div className="flex justify-between">
+        <div className="text-[14px]">
+          <p>Спальных мест: 2</p>
+          <p>Комнат: 1</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-[5px]">
+            <Image src={"/loc.svg"} alt="" width={11} height={14} />
+            <p className="text-[14px] underline opacity-60">Маяк Минска</p>
+          </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
+            <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
+        </div>
+        </div>
+      </div>
+     </div>
+     <div className="px-[17px] shadow-xl w-[370px] pb-[22px] bg-white">
+     <Image
+        className="min-w-[370px] ml-[-17.5px] mb-[6px]"
+        alt=""
+        src={"/third.png"}
+        width={370}
+        height={280}
+      />
+      <h4 className="text-[18px] text-[#458BD1] mb-[10px]">
+      Пр-т Независимости, 52
+      </h4>
+      <div className="flex justify-between">
+        <div className="text-[14px]">
+          <p>Спальных мест: 2</p>
+          <p>Комнат: 1</p>
+        </div>
+        <div>
+          <div className="flex items-center gap-[5px]">
+            <Image src={"/loc.svg"} alt="" width={11} height={14} />
+            <p className="text-[14px] underline opacity-60">Маяк Минска</p>
+          </div>
+        <div className="flex items-center gap-[5px]">
+        <Image src={"/m.svg"} alt="" width={19} height={14} />
             <p className="text-[14px] underline opacity-60">Восток (0,24km)</p>
         </div>
         </div>
